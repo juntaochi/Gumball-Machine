7449a9e73b78a041d7cbcfbd6e56906574adb0c4
Juntao Chi <chijun@iu.edu>
2024-04-24 00:25:06 -0400

    done

---
+++ src/main/java/edu/iu/habahram/GumballMachine/controllers/AuthenticationController.java
@@ -0,0 +1,50 @@
+package edu.iu.habahram.GumballMachine.controllers;
+import edu.iu.habahram.GumballMachine.model.*;
+import edu.iu.habahram.GumballMachine.repository.*;
+import edu.iu.habahram.GumballMachine.security.*;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class AuthenticationController {
+    private final AuthenticationManager authenticationManager;
+
+    private final TokenService tokenService;
+
+    CustomerRepository customerRepository;
+    public AuthenticationController(AuthenticationManager authenticationManager,
+                                    TokenService tokenService, CustomerRepository customerRepository) {
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
+        this.customerRepository = customerRepository;
+    }
+
+
+    @PostMapping("/signup")
+    public void signup(@RequestBody Customer customer) {
+        try {
+            customerRepository.save(customer);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PostMapping("/signin")
+    public String login(@RequestBody Customer customer) {
+        Authentication authentication = authenticationManager
+                .authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                customer.username()
+                                , customer.password()));
+
+        return tokenService.generateToken(authentication);
+    }
+
+
+}
---
+++ src/main/java/edu/iu/habahram/GumballMachine/model/Customer.java
@@ -0,0 +1,7 @@
+package edu.iu.habahram.GumballMachine.model;
+
+public record Customer(String username,
+                       String password,
+                       String email) {
+
+}
